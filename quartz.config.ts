7ef8ab0d6bfd48c914ac4bcda5d172ea2eaecfe1
eniverz <mspt5871@protonmail.com>
2025-04-24 14:38:13 +0800

    update katex options

--- quartz.config.ts
+++ quartz.config.ts
@@ -75,7 +75,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex({ renderEngine: "katex", katexOptions: { displayMode: true } }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
